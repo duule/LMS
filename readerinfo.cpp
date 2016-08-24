615732cf72179b034c80f8b7c498c7b8270644ad
imudgesdule <1326435210@qq.com>
2016-08-24 20:24:54 +0800

    update reader info

--- readerinfo.cpp
+++ readerinfo.cpp
@@ -1,6 +1,8 @@
 #include "readerinfo.h"
 
-ReaderInfo::ReaderInfo(ReaderInfo::ReaderType type)
-{
+ReaderInfo::ReaderInfo(std::__1::string id, std::__1::string name, ReaderInfo::Departments department, ReaderInfo::ReaderType type){
+    this->id = id;
+    this->name = name;
+    this->department =  department;
     this->type = type;
 }
